e97bb1dabd1a813735e755d5a042957835214ef4
Martin Anton <119125486+martinxanton@users.noreply.github.com>
2024-10-23 20:57:29 -0500

    Añade configuración para despliegue en GitHub Pages, actualiza package.json y astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+  site: "https://martinxanton.github.io",
+  base: "portafolio",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
